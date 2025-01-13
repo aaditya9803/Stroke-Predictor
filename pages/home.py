8d19b7fa9539f683f76c606ce65f476627bdc70e
aaditya9803 <aaditya9803@gmail.com>
2025-01-13 17:46:00 +0100

    Made the form

--- pages/home.py
+++ pages/home.py
@@ -1,6 +1,34 @@
 import streamlit as st
 
+def form_submit():
+    print("submitted")
+
+    
+
 def show_home():
+    if 'age'not in st.session_state:
+        st.session_state.age = 0
+    if 'gender'not in st.session_state:
+        st.session_state.gender = ""
+    if 'bmi'not in st.session_state:
+        st.session_state.bmi = 0
+    if 'hypertension'not in st.session_state:
+        st.session_state.hypertension = ""
+    if 'heart_disease'not in st.session_state:
+        st.session_state.hear_disease = ""
+    if 'avg_gulcose'not in st.session_state:
+        st.session_state.avg_gulcose = 0
+    if 'work_type'not in st.session_state:
+        st.session_state.work_type = ""
+    if 'married'not in st.session_state:
+        st.session_state.married = ""
+    if 'smokes'not in st.session_state:
+        st.session_state.smokes = 0
+    if 'residence'not in st.session_state:
+        st.session_state.residence= ""
+    if 'dont_know_gulcose' not in st.session_state:
+        st.session_state.dont_know_gulcose = ""
+
 
     st.markdown("""
     <h1> Stroke Predictor </h1>
@@ -9,63 +37,107 @@ def show_home():
             color: #2c3e50;
             text-align: center;
         }
-        * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        }
-
-        .container {
-        display: grid;
-        grid-template-columns: repeat(3, 1fr);
-        gap: 10px;
-        padding: 10px;
-        }
-
-        .box {
-        border: 2px solid red;
-        padding: 20px;
-        text-align: center;
-        height: 150px;
-        }
-
-        @media screen and (max-width: 768px) {
-        .container {
-            grid-template-columns: repeat(2, 1fr);
-        }
-        }
-
-        @media screen and (max-width: 480px) {
-        .container {
-            grid-template-columns: 1fr;
-        }
-        }
     </style>
     """, unsafe_allow_html=True)
 
 
-    st.markdown('<div class="container">', unsafe_allow_html=True)
+    with st.form(key='my_form'):
+        
+        st.subheader("Gender")
+        st.session_state.gender = st.radio(
+            "Select your gender:",
+            ["Male", "Female"],
+            index=None,
+            help="Choose your gender from the options"
+        )
 
 
 
-    st.markdown('<div class="box">', unsafe_allow_html=True)
-    st.subheader("Gender")
-    gender = st.radio(
-        "Select your gender:",
-        ["Male", "Female", "Other"],
-        index=None,
-        help="Choose your gender from the options"
-    )
-    st.markdown('</div>', unsafe_allow_html=True)
+        st.subheader("Age")
+        st.session_state.age = st.slider("Select your age:",
+                        min_value = 0,
+                        max_value = 100,
+                        value = st.session_state.age,
+        )
 
 
 
-    st.markdown('<div class="box">', unsafe_allow_html=True)
-    st.subheader("Age")
-    age = st.slider("Select your age:",
-                    min_value = 0
-    )
-    st.markdown('</div>', unsafe_allow_html=True)
+        st.subheader("Bmi")
+        st.session_state.bmi = st.slider("Select your bmi:",
+                        min_value = 0,
+                        max_value = 50,
+                        value = st.session_state.bmi,
+        )
 
 
-    st.markdown('</div>', unsafe_allow_html=True)
+        st.subheader("Hypertension")
+        st.session_state.hypertension = st.radio(
+            "Do you have Hypertension?",
+            ["Yes", "No"],
+            index=None,
+            help="If you don't know just select 'No'"
+        )
+
+
+        st.subheader("Heart Disease")
+        st.session_state.heart_disease = st.radio(
+            "Do you have a heart disease?",
+            ["Yes", "No"],
+            index=None,
+            help="If you don't know just select 'No'"
+        )
+
+        st.subheader("Average Gulcose Level")
+        st.session_state.bmi = st.slider("Select your Average Gulcose level:",
+                        min_value = 0,
+                        max_value = 50,
+                        value = st.session_state.bmi,
+                        help = "if you don't know select I don't know"
+        )
+        st.session_state.dont_know_gulcose = st.checkbox(
+            "I don't know my Gulcose Level",
+        )
+
+
+        st.subheader("Married Life")
+        st.session_state.married = st.radio(
+            "Are you married?",
+            ["Yes", "No"],
+            index=None,
+            help="If you don't know just select 'No'"
+        )
+
+
+        st.subheader("Work Type")
+        st.session_state.work_type = st.radio(
+            "Select your work type",
+            ["Private", "Self-employed", "Have children", "Government Job", "Never Worked"],
+            index=None,
+        )
+
+        
+        st.subheader("Residence Type")
+        st.session_state.residence = st.radio(
+            "Select your Residence type",
+            ["Urban", "Rural"],
+            index=None,
+        )
+
+        st.subheader("Smoking Status")
+        st.session_state.smokes = st.radio(
+            "What kind of smoker are you?",
+            ["Smokes", "Formerly Smoked", "Never Smoked", "Unknown"],
+            help="Select Unknown if you aren't sure",
+            index=None,
+        )
+
+
+
+
+
+
+
+        st.subheader("Submit")
+        # if submit_button:
+        #     st.write(f"Form submitted with gender: {gender} and age: {st.session_state.age}")
+        submit_button = st.form_submit_button(label='Submit') #, onclick=form_submit
