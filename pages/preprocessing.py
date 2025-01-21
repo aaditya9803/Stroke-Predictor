5f2014c39e47335fde0fbe8b6a54c9d64e2671ac
aaditya9803 <aaditya9803@gmail.com>
2025-01-21 20:30:27 +0100

    Added file in preprocessing

--- pages/preprocessing.py
+++ pages/preprocessing.py
@@ -1,6 +1,10 @@
 import streamlit as st
-
+import pandas as pd
 def show_preprocessing():
-    st.title('Preprocessing and Analysis on Fake Data')
+    filepath = './static/dataset-stroke.csv'
+    train_data = pd.read_csv((filepath), sep=',', header=0)
+    df = pd.DataFrame(train_data)
+    
 
+    st.title('Preprocessing and Analysis on Fake Data')
     st.header('1. Preprocessing')
