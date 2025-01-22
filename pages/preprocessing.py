b2b43402e7050b5936903b914d9b95a9ba359a33
aaditya9803 <aaditya9803@gmail.com>
2025-01-22 15:22:32 +0100

    removed imports / changed all variables to markdown

--- pages/preprocessing.py
+++ pages/preprocessing.py
@@ -1,12 +1,5 @@
 import streamlit as st
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-from sklearn.preprocessing import LabelEncoder
-from collections import Counter
-from imblearn.over_sampling import SMOTE
-from imblearn.under_sampling import RandomUnderSampler
-from imblearn.pipeline import Pipeline
+
 
 def show_preprocessing():
 
@@ -15,24 +8,22 @@ def show_preprocessing():
     # .stMarkdown { margin-bottom: 5px; }  /* Reduce bottom margin */
     # </style>
     # """, unsafe_allow_html=True)
-    filepath = './static/dataset-stroke.csv'
-    train_data = pd.read_csv((filepath), sep=',', header=0)
-    df = pd.DataFrame(train_data)  #this is orginal dataframe
-    df_stroke = df[df['stroke'] == 1] # this is dataframe of rows only where stroke was 1
-    df_stroke = df_stroke.drop(columns='id')
-
 
     st.title('Preprocessing, ML Models, and Analysis on Fake Data')
+    st.header(" ")
+#Preprocessing
     st.header('1. Preprocessing')
-
-
-    st.subheader('1.1. NaN Values')
-    df['bmi']=df['bmi'].fillna(df['bmi'].mean())
+#Null Values
+    st.subheader('1.1. Null Values')
     st.code("df['bmi'].isnull().sum()")
-    st.markdown(f"There were {df['bmi'].isnull().sum()} Null values in the bmi column")
+    st.markdown(f"There were 201 Null values in the bmi column")
     st.code("df['bmi'].fillna(df['bmi'].mean(), inplace=True)")
-    st.markdown(f"These Null values were then replaced by the mean({df['bmi'].isnull().sum()}) of the column")
+    st.markdown(f"These Null values were then replaced by the mean(28.89) of the column")
 
+
+
+# Label Encoder
+    st.subheader(" ")
     st.subheader('1.2. Label Encoder')
     st.code("""
         label_encoder = LabelEncoder()
@@ -42,36 +33,39 @@ def show_preprocessing():
             df[column] = label_encoder.fit_transform(df[column])
         """)
 
-
-
     st.markdown("The non-numerical values in the dataframe was encoded as numerical values in the following ways:")
-    label_encoder = LabelEncoder()
-    columns_to_label = ['gender', 'ever_married', 'work_type', 'Residence_type', 'smoking_status']
-    for column in columns_to_label:
-        label_encoder.fit(df[column])
-        st.markdown(f"• Column: {column}")
-        rows = []
-        for label, encoded in zip(label_encoder.classes_, range(len(label_encoder.classes_))):
-            rows.append(f"|    {label} → {encoded}    |")
-        st.markdown("\n".join(rows), unsafe_allow_html=True)
+    
+    st.code("""
+    
+        • Column: gender
 
-    #To actually label for sampling and to not be shown on webpage
-    # df_stroke is labelled here aswell
-    columns_to_label = ['gender', 'ever_married', 'work_type', 'Residence_type', 'smoking_status']
-    for column in columns_to_label:
-        df[column] = df[column].astype(str)
-        df_stroke[column] = df_stroke[column].astype(str)
-        df[column] = label_encoder.fit_transform(df[column])
-        df_stroke[column] = label_encoder.fit_transform(df_stroke[column])
+        | Female → 0 | | Male → 1 | | Other → 2 |
 
+        • Column: ever_married
 
+        | No → 0 | | Yes → 1 |
 
+        • Column: work_type
+
+        | Govt_job → 0 | | Never_worked → 1 | | Private → 2 | | Self-employed → 3 | | children → 4 |
+
+        • Column: Residence_type
+
+        | Rural → 0 | | Urban → 1 |
+
+        • Column: smoking_status
+
+        | Unknown → 0 | | formerly smoked → 1 | | never smoked → 2 | | smokes → 3 |    
+            
+    
+    """)
+
+# OverSampling and UnderSampling
+    st.subheader(" ")
     st.subheader('1.3. Oversampling/Undersampling')
-    y = df['stroke']
-    X = df.drop(columns='stroke')
     st.markdown("There is class imbalance in the dataframe:")
-    st.markdown(f"• stroke == 0 is {Counter(y)[0]}")
-    st.markdown(f"• stroke == 1 is {Counter(y)[1]}")
+    st.markdown(f"• stroke == 0 is 4861")
+    st.markdown(f"• stroke == 1 is 249")
     st.code("""
             over = SMOTE(sampling_strategy = 1)
             under = RandomUnderSampler(sampling_strategy = 0.1)
@@ -80,23 +74,16 @@ def show_preprocessing():
             X, y = pipeline.fit_resample(X, y)
             """)
     st.markdown("So, the stroke negative class was halved using RandomUnderSampler() and stroke positive class was Doubled using SMOTE()")
-    
-    over = SMOTE(sampling_strategy = 1)
-    under = RandomUnderSampler(sampling_strategy = 0.1)
-    steps = [('under', under),('over', over)]
-    pipeline = Pipeline(steps=steps)
-    X, y = pipeline.fit_resample(X, y)
-
     st.markdown("When the dataframe is balanced, it looks as follows")
-    st.markdown(f"• stroke == 0 is {Counter(y)[0]}")
-    st.markdown(f"• stroke == 1 is {Counter(y)[1]}")
+    st.markdown(f"• stroke == 0 is 2490")
+    st.markdown(f"• stroke == 1 is 2490")
 
 
-#SVM
+
     st.header(" ")
     st.header("2. Machine Learning Models")
+#SVM
     st.subheader("2.1. Support Vector Machine")
-
 #Cross Fold Method SVM   
     st.markdown("##### 2.1.1 Cross Fold Method")
     st.code("""
@@ -107,6 +94,7 @@ def show_preprocessing():
             """)
 
 # Test Train Split SVM
+    st.subheader(" ")
     st.markdown("##### 2.1.2 Test-Train Split Method")
     st.code("""
         Final Model Accuracy: 0.7590
@@ -120,7 +108,7 @@ def show_preprocessing():
 
 
 #XGBoost model
-    st.header(" ")
+    st.subheader(" ")
     st.subheader("2.2. XGBoost")
     st.markdown("**Note: Since the XGBoost Model seemed promising, it is primarily used in this project**")
 # Cross Fold Method XGB
@@ -158,7 +146,7 @@ def show_preprocessing():
             [[1237    6]
             [   4 1243]]
         """)
-    st.markdown("Note: Since the final model was trained on whole dataframe, higher values on performance report means the data is already seen which doesn't implies that model is overfitting")
+    st.markdown("Note: Since the final model was trained on whole dataframe, higher values on performance report means the data is already seen which doesn't implies that the model is overfitting")
 
 
 # Testing the Final model with only Stroke dataset
@@ -173,4 +161,5 @@ def show_preprocessing():
             [[  0   0]
             [  7 242]]
         """)
-    st.markdown("Note: Since the final model was trained on whole dataframe, higher values on performance report means the data is already seen which doesn't implies that model is overfitting")
+    st.markdown("Note: Since the final model was trained on whole dataframe, higher values on performance report means the data is already seen which doesn't implies that the model is overfitting")
+
